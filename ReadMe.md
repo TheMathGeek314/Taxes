e7a8a8b611cb3a6cab7d5bdd08e354f3fdd7c9e7
TheMathGeek_314 <16294237+TheMathGeek314@users.noreply.github.com>
2024-11-16 21:15:44 -0600

    Add project files.

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+# Taxes
+
+A mod for the game Hollow Knight.
+
+All your geo means all your geo. The basin fountain vessel fragment still requires 3000 minimum, but you are only given the option to drop ALL geo. No cost too great.
---
+++ Taxes.cs
@@ -0,0 +1,33 @@
+﻿using Modding;
+using System.Collections.Generic;
+using UnityEngine;
+using Satchel;
+using HutongGames.PlayMaker;
+
+namespace Taxes {
+    public class Taxes: Mod {
+        new public string GetName() => "Taxes";
+        public override string GetVersion() => "1.0.0.0";
+
+        public override void Initialize(Dictionary<string, Dictionary<string, GameObject>> preloadedObjects) {
+            On.PlayMakerFSM.OnEnable += editFSM;
+        }
+
+        private void editFSM(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self) {
+            orig(self);
+            if(self.gameObject.name.Contains("Fountain Donation") && self.FsmName == "Conversation Control") {
+                self.GetValidState("Full Donation").InsertAction(new takeAllGeo(self), 0);
+            }
+        }
+    }
+
+    public class takeAllGeo: FsmStateAction {
+        private PlayMakerFSM self;
+        public takeAllGeo(PlayMakerFSM self) {
+            this.self = self;
+        }
+        public override void OnEnter() {
+            self.FsmVariables.GetFsmInt("Amount to Donate").Value = PlayerData.instance.geo;
+        }
+    }
+}
